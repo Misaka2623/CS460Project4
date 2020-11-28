8fe02bfdf91edeaa723c4b546283fc62b5991938
Misaka2623 <415470991@qq.com>
2020-11-28 13:01:48 -0700

    add some commets to html

--- src/main/java/main/AppController.java
+++ src/main/java/main/AppController.java
@@ -5,7 +5,6 @@ import bean.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
