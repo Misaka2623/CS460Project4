ddb2e7275399151d1e837d55e7920ab0ed685d74
Misaka2623 <415470991@qq.com>
2020-12-01 22:19:10 -0700

    Update MemberDao.java

--- src/dao/MemberDao.java
+++ src/dao/MemberDao.java
@@ -9,4 +9,13 @@ public interface MemberDao {
      * @return {@code true} if the username and password pair is in the member table; {@code false} otherwise.
      */
     boolean checkUsernameAndPassword(String username, String password);
+
+    /**
+     * check whether the specified username is in the member table.
+     *
+     * @param username the username that should be checked.
+     * @return {@code true} if there is a member's username that matches the specified username; {@code false}
+     * otherwise.
+     */
+    boolean containsUsername(String username);
 }
