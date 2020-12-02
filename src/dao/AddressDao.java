44e4e5cf8d02db724ef67d07701902f41ac604d4
Misaka2623 <415470991@qq.com>
2020-12-01 18:13:36 -0700

    add javadoc

--- src/dao/AddressDao.java
+++ src/dao/AddressDao.java
@@ -3,5 +3,13 @@ package dao;
 import bean.Address;
 
 public interface AddressDao {
+    /**
+     * insert a line of specified address object into the table. some fields of the specified address might be empty.
+     * the id field of the specified address is not filled, so to implement this method should find the appropriate
+     * id of this address.
+     *
+     * @param address the address that should be inserted.
+     * @return the id of the address.
+     */
     long insert(Address address);
 }
--- src/dao/BaseDao.java
+++ src/dao/BaseDao.java
@@ -6,20 +6,47 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * this is a basic data access object for connecting to jdbc.
+ */
 public abstract class BaseDao {
-    private static final String DRIVER = "oracle.jdbc.OracleDriver";
-    private static final String URL = "jdbc:oracle:thin:@aloe.cs.arizona.edu:1521:oracle";
-    private static final String USERNAME = "shenghou";
-    private static final String PASSWORD = "a9397";
+    /**
+     * the sql driver used in this project.
+     */
+    private static final String DATABASE_DRIVER = "oracle.jdbc.OracleDriver";
 
+    /**
+     * the url of the database.
+     */
+    private static final String DATABASE_URL = "jdbc:oracle:thin:@aloe.cs.arizona.edu:1521:oracle";
+
+    /**
+     * the username of the database.
+     */
+    private static final String DATABASE_USERNAME = "shenghou";
+
+    /**
+     * the password of the database.
+     */
+    private static final String DATABASE_PASSWORD = "a9397";
+
+    // initialize the database at the first time that this class is loaded.
     static {
         try {
-            Class.forName(DRIVER);
+            Class.forName(DATABASE_DRIVER);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * execute a sql statement and get the results.
+     *
+     * @param preparedSql an SQL statement that may contain one or more '?' IN parameter placeholders
+     * @param args        the arguments to replace the '?' in prepared statement.
+     * @return a {@code ResultSet} object that contains the data produced by the query; {@code null} if some errors
+     * happened when executing the sql.
+     */
     public ResultSet executeSql(String preparedSql, Object... args) {
         try (Connection connection = getConnection()) {
             try (PreparedStatement statement = connection.prepareStatement(preparedSql)) {
@@ -34,6 +61,11 @@ public abstract class BaseDao {
         return null;
     }
 
+    /**
+     * generate an new id by find the largest id in the sql and add 1 to it.
+     *
+     * @return the generated id.
+     */
     protected long generateId() {
         String tableName = getTableName();
         try (ResultSet resultSet = executeSql("SELECT max(\"?\") FROM \"?\"", tableName + "_id", tableName)) {
@@ -46,9 +78,20 @@ public abstract class BaseDao {
         return 1;
     }
 
+    /**
+     * get the name of the table that this object access to.
+     *
+     * @return the name of the table.
+     */
     protected abstract String getTableName();
 
+    /**
+     * get the connection of the database.
+     *
+     * @return a connection to the database.
+     * @throws SQLException if a database access error occurs.
+     */
     protected Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        return DriverManager.getConnection(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
     }
 }
