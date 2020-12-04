9329043ee9a05445fac6bcf2eb49e4d3b15aa543
Misaka2623 <415470991@qq.com>
2020-12-03 23:48:10 -0700

    Update ProductDao.java

--- src/dao/ProductDao.java
+++ src/dao/ProductDao.java
@@ -14,6 +14,14 @@ public interface ProductDao {
     int calculateProductAmount(long productId);
 
     /**
+     * get the price of the product whose id is {@code productId}.
+     *
+     * @param productId the id of the product.
+     * @return the price of the product.
+     */
+    double getPriceOfProduct(long productId);
+
+    /**
      * grab all products which have at least 1 in stock into a list.
      *
      * @return a map that contains all products in the database. It's key is a product, and it's value is the amount
