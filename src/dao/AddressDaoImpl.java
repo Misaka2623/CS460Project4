af01331369798eddfdc0eb6529e196227b4fd119
he1l0world <hellochen19970916@gmail.com>
2020-12-06 00:18:52 -0700

    implement the address insert

--- src/dao/AddressDaoImpl.java
+++ src/dao/AddressDaoImpl.java
@@ -7,6 +7,22 @@ public class AddressDaoImpl extends BaseDao implements AddressDao {
     public long insert(Address address) {
         address.setAddressId(generateId());
         // TODO: 12/1/20 insert address into database
+        // if id, line1, city, state, post code has none then return -1;
+        Connection connect = getConnection();
+        long id = address.getAddressId();
+        String line1 = address.getLine1();
+        String line2 = address.getLine2();
+        String line3 = address.getLine3();
+        String city = address.getCity();
+        String state = address.getState();
+        String post_code = address.getPostalCode();
+
+        if(id == none || line1 == none || city == none || state == none || post_code == none)
+            return -1;
+
+        String query = "INSERT INTO address VALUES (id,line1,line2,line3,city,state,post_code);";
+        executeSql(query);
+
         return address.getAddressId();
     }
 
