8a2ffee7081e74875d4742520c6e290d7386666c
he1l0world <hellochen19970916@gmail.com>
2020-12-06 03:23:02 -0700

    start sale

--- src/dao/SaleDaoImpl.java
+++ src/dao/SaleDaoImpl.java
@@ -1,4 +1,15 @@
 package dao;
 
 public class SaleDaoImpl extends BaseDao implements SaleDao {
+    @Override
+    public long insert(long memberId, String paymentMethod, Map<Long, Integer> cart){
+        //TODO: insert sale table and sub_sale table
+        return 0;
+    }
+
+    @Override
+    public  double getTotalPriceById(long saleId){
+        //TODO: get total price
+        return 0.0;
+    }
 }
