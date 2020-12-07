7d6da3fd972740ac722e84c343c6677a368a17e0
w1607 <w1607@DESKTOP-EJI02NC>
2020-12-07 05:03:43 -0700

    warehouse

--- src/dao/WarehouseDaoImpl.java
+++ src/dao/WarehouseDaoImpl.java
@@ -1,4 +1,14 @@
 package dao;
 
+import bean.Warehouse;
+
 public class WarehouseDaoImpl extends BaseDao implements WarehouseDao {
+	/**
+	 * insert a new line to add a specified product
+	 * 
+	 * @param a specified product should be inserted
+	 * @return the ID of the product
+	 */
+	
+	long insert(Warehouse warehouse);
 }
