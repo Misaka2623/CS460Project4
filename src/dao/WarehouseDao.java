7a88ea81d998ffe4e2e2d5c5def3ebdad0c19b19
w1607 <w1607@DESKTOP-EJI02NC>
2020-12-07 05:37:10 -0700

    warehouse draft

--- src/dao/WarehouseDao.java
+++ src/dao/WarehouseDao.java
@@ -1,4 +1,37 @@
 package dao;
 
+import bean.Warehouse;
+
 public interface WarehouseDao {
+	/**
+	 * insert a new line to add a specified product
+	 * 
+	 * @param a specified product should be inserted
+	 * @return the ID of the product
+	 */
+	long insert(Warehouse warehouse);
+	
+	/**
+	 * insert a new line to add a specified product
+	 * 
+	 * @param warehouseId Long Integer Related warehouse ID
+	 * @return whether it's success or failure
+	 */
+	boolean delete(long warehouseId);
+	
+	/**
+	 * Record of the incoming date of a specified product
+	 * 
+	 * @param warehouseId
+	 * @return incomingDate
+	 */
+	Date getIncomeDate(Date incomingDate);
+	
+	/**
+	 * the recording of a purchase price for the goods
+	 * 
+	 * @param warehouseId
+	 * @return purchasePrice
+	 */
+	double getPurchasePrice(double purchasePrice);
 }
