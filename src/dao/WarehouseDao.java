2b528814f923b0e40f94e68a192b88811941a96f
w1607 <w1607@DESKTOP-EJI02NC>
2020-12-07 05:56:52 -0700

    warehouse

--- src/dao/WarehouseDao.java
+++ src/dao/WarehouseDao.java
@@ -18,20 +18,4 @@ public interface WarehouseDao {
 	 * @return whether it's success or failure
 	 */
 	boolean delete(long warehouseId);
-	
-	/**
-	 * Record of the incoming date of a specified product
-	 * 
-	 * @param warehouseId
-	 * @return incomingDate
-	 */
-	Date getIncomeDate(Date incomingDate);
-	
-	/**
-	 * the recording of a purchase price for the goods
-	 * 
-	 * @param warehouseId
-	 * @return purchasePrice
-	 */
-	double getPurchasePrice(double purchasePrice);
 }
--- src/dao/WarehouseDaoImpl.java
+++ src/dao/WarehouseDaoImpl.java
@@ -1,4 +1,21 @@
 package dao;
 
 public class WarehouseDaoImpl extends BaseDao implements WarehouseDao {
+	@Override
+	public long insert(Warehouse warehouse) {
+		// return warehouseId
+		warehouseID.setWarehouseId(generateId());
+		long warehouse_id = warehouse.getWarehouseId();
+		long supplierId = warehouse.getSupplierId();
+		long productId = warehouse.getProductId();
+		Date incomingDate = warehouse.getIncomingDate();
+		double purchasePrice = warehouse.getPurchasePrice();
+        int amount = warehouse.getAmount();
+        
+        if(amount == null) {
+        	return -1
+        }
+        
+        String query = String.format("INSERT INFO warehouse VALUES (d%, d%, s%, d%, d%)", supplierId, productId, incomingDate, purchasePrice, amount )
+	}
 }
