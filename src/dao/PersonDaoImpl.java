8a7aff12e2308f7fba0246f25aec78c6ed4508d3
Suyang Chen <hellochen19970916@gmail.com>
2020-12-08 05:37:05 -0700

    update the person sql

--- src/dao/PersonDaoImpl.java
+++ src/dao/PersonDaoImpl.java
@@ -6,21 +6,14 @@ public class PersonDaoImpl extends BaseDao implements PersonDao {
     @Override
     public long insert(Person person){
         // return personId
-        person.setPersonId(generateId());
-        long person_id = person.getPersonId();
-        String first_name = person.getFirstName();
-        String last_name = person.getLastName();
-        Gender gender = person.getGender();
-        Date birthday = person.getBirthday();
-        long address_id = person.getAddressId();
-        String phone = person.getPhone();
 
-        if( first_name == null || last_name  == null || gender == null)
+        if( person.getFirstName() == null || person.getLastName()  == null || person.getGender() == null)
             return -1;
 
         //TODO: Date object
-        String query = String.format("INSERT INTO person VALUES (%d,%s,%s,%d,%s,%d,%d)", person_id, first_name, last_name, gender.getValue(), birthday, address_id, phone);
-        ResultSet answer = executeSql(query);
+        ResultSet answer = executeSql("INSERT INTO \"person\" VALUES (?,?,?,?,?,?,?)",
+                person.getPersonId(), person.getFirstName(), person.getLastName(), person.getGender().getValue(), person.getBirthday(),
+                person.getAddressId(), person.getPhone());
 
         return person.getPersonId();
     }
