0d9029bc79b34008dfae23b9aa2ebb91930db5b5
he1l0world <hellochen19970916@gmail.com>
2020-12-08 06:28:18 -0700

    implement category

--- src/dao/CategoryDaoImpl.java
+++ src/dao/CategoryDaoImpl.java
@@ -1,4 +1,32 @@
 package dao;
 
 public class CategoryDaoImpl extends BaseDao implements CategoryDao {
+    public long insert(Category category){
+        category.setCategoryId(generateId());
+
+        if( category.getName() == null)
+            return 0;
+        ResultSet answer = executeSql("INSERT INTO \"category\" VALUES (?,?)",
+        category.getCategoryId(), category.getName());
+
+        return category.getCategoryId();
+    }
+
+    public boolean delete(long categoryId){
+        //TODO: check if delete sucess
+        ResultSet answer = executeSql("DELETE FROM category WHERE category_id = (?)", categoryId);
+        return true;
+    }
+
+    public long insert(String name){
+        long categoryId = generateId();
+        ResultSet answer = executeSql("INSERT INTO \"category\" VALUES (?,?)",categoryId, name);
+        return categoryId;
+    }
+
+    public List<Category> getAll(){
+        //TODO: implement getAll
+        List<Category> all = new List<category>();
+        return all;
+    }
 }
