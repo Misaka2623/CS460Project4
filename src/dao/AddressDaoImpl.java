1ee1011be00bf18b2f40d3a112286026f4283b4f
Misaka2623 <415470991@qq.com>
2020-12-10 02:27:00 -0700

    Update AddressDaoImpl.java

--- src/dao/AddressDaoImpl.java
+++ src/dao/AddressDaoImpl.java
@@ -2,6 +2,11 @@ package dao;
 
 import bean.Address;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+
 public class AddressDaoImpl extends BaseDao implements AddressDao {
     @Override
     public long insert(Address address) {
@@ -12,6 +17,33 @@ public class AddressDaoImpl extends BaseDao implements AddressDao {
                 address.getState(), address.getPostalCode());
 
         return address.getAddressId();
+    }
+
+    @Override
+    public boolean delete(long addressId) {
+        return executeUpdate("DELETE FROM \"address\" WHERE \"address_id\"=?", addressId) != 0;
+    }
+
+    @Override
+    public List<Address> getAll() {
+        try (ResultSet resultSet = executeSql("SELECT * FROM \"address\"")) {
+            List<Address> addresses = new LinkedList<>();
+            while (resultSet.next()) {
+                Address address = new Address();
+                address.setAddressId(resultSet.getLong("address_id"));
+                address.setLine1(resultSet.getString("line1"));
+                address.setLine2(resultSet.getString("line2"));
+                address.setLine3(resultSet.getString("line3"));
+                address.setCity(resultSet.getString("city"));
+                address.setState(resultSet.getString("state"));
+                address.setPostalCode(resultSet.getString("postal_code"));
+                addresses.add(address);
+            }
+            return addresses;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
