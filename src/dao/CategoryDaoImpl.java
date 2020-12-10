af6f26b83f3ebf2ea7017025a4a77bf246fa6c5f
Misaka2623 <415470991@qq.com>
2020-12-10 10:10:21 -0700

    Update CategoryDaoImpl.java

--- src/dao/CategoryDaoImpl.java
+++ src/dao/CategoryDaoImpl.java
@@ -1,42 +1,54 @@
 package dao;
 
+import bean.Category;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+
 public class CategoryDaoImpl extends BaseDao implements CategoryDao {
-    public long insert(Category category){
+    public long insert(Category category) {
         category.setCategoryId(generateId());
 
-        if( category.getName() == null)
+        if (category.getName() == null) {
             return 0;
+        }
         ResultSet answer = executeSql("INSERT INTO \"category\" VALUES (?,?)",
-        category.getCategoryId(), category.getName());
+                category.getCategoryId(), category.getName());
 
         return category.getCategoryId();
     }
 
-    public boolean delete(long categoryId){
-        //TODO: check if delete sucess
+    public boolean delete(long categoryId) {
         int answer = executeUpdate("DELETE FROM category WHERE category_id = (?)", categoryId);
-        if(answer == 0)
-            return false;
-        return true;
+        return answer != 0;
     }
 
-    public long insert(String name){
+    public long insert(String name) {
         long categoryId = generateId();
-        ResultSet answer = executeSql("INSERT INTO \"category\" VALUES (?,?)",categoryId, name);
+        ResultSet answer = executeSql("INSERT INTO \"category\" VALUES (?,?)", categoryId, name);
         return categoryId;
     }
 
-    public List<Category> getAll(){
+    public List<Category> getAll() {
         //TODO: implement getAll
-        List<Category> all = new List<category>();
-        Result answer = executeSql("SELECT * from category");
-        while(answer.next())
-        {
-            long id = answer.getLong();
-            String name = answer.getString();
-            Category each = Category(id, name);
-            all.add(each);
+        List<Category> all = new LinkedList<>();
+        try (ResultSet answer = executeSql("SELECT * from category")) {
+            while (answer.next()) {
+                long id = answer.getLong("category_id");
+                String name = answer.getString("name");
+                Category each = new Category(id, name);
+                all.add(each);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         return all;
+    }
+
+    @Override
+    protected String getTableName() {
+        return Category.class.getSimpleName().toLowerCase();
     }
 }
