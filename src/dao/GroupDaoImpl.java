84fdc422071b0e8dc4d7d76db8b6bd30a5fb7866
he1l0world <hellochen19970916@gmail.com>
2020-12-10 03:42:48 -0700

    implement group table

--- src/dao/GroupDaoImpl.java
+++ src/dao/GroupDaoImpl.java
@@ -1,4 +1,34 @@
 package dao;
 
 public class GroupDaoImpl extends BaseDao implements GroupDao {
+    public long insert(Group group){
+        group.setGroupId(generateId());
+        if( group.getName() == null)
+            return 0;
+
+        ResultSet answer = executeSql("INSERT INTO \"group\" VALUES (?,?)",
+        group.getGroupId(), group.getName());
+
+        return  group.getGroupId() ;
+    }
+
+    public boolean delete(long groupId){
+        int answer = executeUpdate("DELETE FROM \"group\"  WHERE group_id = (?)", groupId);
+        if(answer == 0)
+            return false;
+        return true;
+    }
+
+    public List<Group> getAll(){
+        List<employee> total = new List<Employee>();
+        Result answer = executeSql("SELECT * from \"group\" ");
+        while(answer.next())
+        {
+            long id = answer.getLong();
+            String namee = answer.getString();
+            Category each = Category(id, names);
+            total.add(each);
+        }
+        return total;
+    }
 }
